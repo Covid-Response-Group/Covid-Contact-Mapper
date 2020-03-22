ddbb7a70c98d8fab1b7c6623465d6f1961f1f62b
Abhinav Tripathi <abhinavtripathi01@hotmail.com>
2020-03-22 15:10:28 +0530

    Location permission handling

--- app/src/main/java/com/wordingly/covidcontacttracer/MainActivity.java
+++ app/src/main/java/com/wordingly/covidcontacttracer/MainActivity.java
@@ -1,6 +1,11 @@
 package com.wordingly.covidcontacttracer;
 
+import android.Manifest;
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +14,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -16,11 +23,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener{
     GoogleSignInClient mGoogleSignInClient;
     private static final int RC_SIGN_IN = 8476;
+    private static final int PERMISSION_FINE_LOCATION = 8477;
     private static final String TAG = "MainActivity-SignIn";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,12 +70,80 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         updateUI(account);
     }
 
+
     private void updateUI(GoogleSignInAccount account) {
         // If user signed in, hide Sign In button and move to next screen
         if (account != null) {
             findViewById(R.id.btn_google_signin).setVisibility(View.GONE);
             // Go to next screen
         }
+    }
+
+    private void checkAskPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                showPermissionRequestDialog();
+            }
+        } else {
+            // Permission has already been granted
+            askPermission();
+        }
+    }
+
+    private void askPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_FINE_LOCATION);
+    }
+
+    private void showPermissionRequestDialog() {
+        final AlertDialog alertDialog = new AlertDialog.Builder(
+                this).create();
+
+        // Setting Dialog Title
+        alertDialog.setTitle("Location Permission Request");
+
+        // Setting Dialog Message
+        alertDialog.setMessage(this.getResources().getString(R.string.loaction_permission_message));
+
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Allow Location Access", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                askPermission();
+                alertDialog.dismiss();
+            }
+        });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Deny Location Access", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                final Snackbar snackbar = Snackbar.make(findViewById(R.id.parent), "Location Permission Mandatory", Snackbar.LENGTH_INDEFINITE);
+                snackbar.setAction("Allow", new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        askPermission();
+                        snackbar.dismiss();
+                        alertDialog.dismiss();
+                    }
+                });
+                snackbar.show();
+            }
+        });
+
+
+        // Showing Alert Message
+        alertDialog.show();
     }
 
 
@@ -100,5 +178,11 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
             updateUI(null);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkAskPermission();
     }
 }
